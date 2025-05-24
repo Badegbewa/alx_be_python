e0737c12bff7fa9a9cf51cfe0da341d2c7aea1fb
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-05-24 18:27:04 +0100

    calculation of yearly interest projection on annual salary

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,1 +1,8 @@
-
+monthly_income = input("Enter your monthly income: 5000 ")
+monthly_expenses = input("Enter your total monthly expenses: 4000 ")
+monthly_income = float(5000)
+monthly_expenses = float(4000)
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"your monthly savings are ${monthly_savings}.")
+print(f"projected savings after one year, with interest, is: ${projected_savings}")
