162f5df825f4a55718a0233aac53bccf9b1006cb
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-06-22 19:07:14 +0100

    created a class Book to write title,authour and year using main.py to run code

---
+++ oop/book_class.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self,title,authour,year):
+        self.title = title
+        self.authour = authour
+        self.year = year
+    
+    def __del__(self):
+        print("Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.authour}, published in {self.year}"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.authour}', {self.year})"
+    
